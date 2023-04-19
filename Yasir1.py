19a0ffcf31537910a6930443e0ee99c1e9257b12
Yasir387 <102988576+Yasir387@users.noreply.github.com>
2023-04-19 18:01:18 +0500

    Create Yasir1.py

---
+++ Yasir1.py
@@ -0,0 +1,17 @@
+import os,re,sys,platform
+ 
+os.system('git pull')
+ 
+os.system('pkg install play-audio')
+ 
+import requests
+ 
+ 
+ 
+bit = platform.architecture()[0]
+ 
+if bit == '64bit':
+ 
+    from qun import uid
+ 
+    Main()
